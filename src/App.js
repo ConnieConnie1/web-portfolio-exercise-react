f6adb742b3ab9142292c29768e30928ec4433d75
ConnieConnie1 <costanza.gorick@gmail.com>
2024-01-19 11:50:51 +0100

    Banner
    Added banner component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
-// import { Banner } from './components/Banner';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <Banner /> */}
+      <Banner />
     </div>
   );
 }
