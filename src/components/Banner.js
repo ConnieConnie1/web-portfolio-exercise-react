70511871f3ac0ee844d86b3a94a199ed56a84b65
ConnieConnie1 <costanza.gorick@gmail.com>
2024-01-22 17:07:12 +0100

    Update Banner with explanation comments

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,8 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+  // Definizione degli stati del componente
+
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [index, setIndex] = useState(1);
@@ -12,6 +14,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
 
+  // Questo è un effetto collaterale che cambierà ogni volta che cambierà o text o delta, le due variabili che passo tra parentesi quadre
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
@@ -22,26 +25,29 @@ export const Banner = () => {
     };
   }, [text, delta]);
 
+  // Questa è la funzione 'tick' che gestisce l'animazione dinamica del testo
   const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
+    let i = loopNum % toRotate.length; //Calcolo col modulo un indice, che rimane all'interno del range delle posizioni disponibili all'interno dell'Array 'toRotate'
+    let fullText = toRotate[i]; // Questo è il testo completo da visualizzare
+    // Testo visualizzato aggiornato, in base a se si stanno o no cancellando i caratteri, di conseguenza aggiunge/toglie caratteri
     let updatedText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+    setText(updatedText); //Settagio del testo. 
 
+    // Caso in cui il testo è vuoto
     if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     }
-
+    // Questo è il caso in cui il testo è completo
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
       setDelta(period);
-    } else if (isDeleting && updatedText === "") {
+    } else if (isDeleting && updatedText === "") { // questa è in fase di cancellazione, ma il testo vuoto
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
@@ -49,9 +55,9 @@ export const Banner = () => {
     }
 
     if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2); // Se sta cancellando, il tempo dimezza (tempo delta) per rendere la cancellazione più rapida
     } else {
-      setIndex((prevIndex) => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1); //Sennò viene incrementato l'indice al prossimo elemento dell'array toRotate
     }
   };
 
